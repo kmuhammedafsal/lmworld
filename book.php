b6ff2efa44afc382da9eee796cdab87a5cf9e9ae
kmuhammedafsal <mafsalk8@gmail.com>
2021-05-31 20:53:27 +0530

    m

---
+++ book.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="icon" href="https://images.unsplash.com/photo-1620288627223-53302f4e8c74?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGFtYm9yZ2hpbmklMjBsb2dvfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60.svg" type="image/icon type">
+</head>
+<body >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php"><strong>LAMBHORGHINI</strong> Motorrad </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="models.php">Models</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="book.php">Bookings</a>
+              </li>
+             
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="https://1000logos.net/wp-content/uploads/2018/03/Symbol-Lamborghini.jpg" alt="" width="140" height="35" class="d-inline-block align-top">
+       <strong>S</strong>heer <strong>D</strong>riving <strong>P</strong>leasure.
+      </a>
+    </div>
+  </nav>
+    
+
+    <div class="container">
+        <div class="row">
+          <div class="col">
+            <h3><center></center></h3>
+          </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <table class="table table-borderless" style="background-color: #000000; color: #ffffff;">
+                    <tr>
+                    <br><br>
+                        <td colspan="2"><center><h3>Booking Details</h3></center></td><br><br><br>
+                    </tr>
+                
+                <tr>
+                    <td>Customer Name</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Vehicle Color</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Vehicle Model</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Customer ID</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Date of Booking</td>
+                    <td><input class="form-control" type="date" name="" id=""></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                <tr>
+                    <td>Mobile</td>
+                    <td><input class="form-control"type="text"></td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td><Button class="btn btn-outline-light" >Confirm</Button></td>
+                </tr>
+            </table></div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="icon" href="https://images.unsplash.com/photo-1620288627223-53302f4e8c74?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGFtYm9yZ2hpbmklMjBsb2dvfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60.svg" type="image/icon type">
+</head>
+<body >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php"><strong>LAMBHORGHINI</strong> Motorrad </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="models.php">Models</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="book.php">Bookings</a>
+              </li>
+             
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="https://1000logos.net/wp-content/uploads/2018/03/Symbol-Lamborghini.jpg" alt="" width="140" height="35" class="d-inline-block align-top">
+       <strong>S</strong>heer <strong>D</strong>riving <strong>P</strong>leasure.
+      </a>
+    </div>
+  </nav>
+  <div class="container">
+    
+      <div class="row">
+          <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <table class="table table-borderless">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="https://images.unsplash.com/photo-1552176625-e47ff529b595?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>Masculine X Series in The Stocks</h5>
+                          No matter what challenges may lie in your way, the BMW X series eagerly anticipates them.
+                          
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://images.unsplash.com/photo-1571607388263-1044f9ea01dd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=686&q=80" class="d-block w-100" alt="..." >
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>Dashing Z-series Book Now</h5>
+                          Superiority in every discipline: the BMW Z Automobiles manifest their claim to leadership with powerful proportions
+                           </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://images.unsplash.com/photo-1544829099-b9a0c07fad1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" class="d-block w-100" alt="..." >
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>The All New BMW M2 Competition</h5>
+                          When dynamics meet luxury, the result is something unique.
+                           </div>
+                      </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+
+              </table>
+            </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col">
+          <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Models</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Body Types</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Gallery</button>
+            </li>
+          </ul>
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col"><h3><center>UPCOMING MODELS</center></h3></div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col">
+          <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1599135343721-6c8d4c1b8b58?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1236&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Born Electric</h5>
+                  <p class="card-text">i3</p>
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Pre-Order Now
+                  </button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Customer Agreement</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        I Here by acccept to terms and conditions of BMW inc. and Proceed to Booking Page.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a class="btn btn-primary" href="book.php" role="button">Understood</a>
+      </div>
+    </div>
+  </div>
+</div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1543854589-fdd815f176e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=607&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Prepare To Prevoke</h5>
+                  <p class="card-text">420i M Sport Coupe</p>
+                  <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Pre-Order Now
+                  </button>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel">Customer Agreement</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          I Here by acccept to terms and conditions of BMW inc. and Proceed to Booking Page.
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <a class="btn btn-primary" href="book.php" role="button">Understood</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Refined Yet Athletic</h5>
+                  <p class="card-text">520i M Sport Saloon</p>
+                  <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Pre-Order Now
+                  </button>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel">Customer Agreement</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          I Here by acccept to terms and conditions of BMW inc. and Proceed to Booking Page.
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <a class="btn btn-primary" href="book.php" role="button">Understood</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col">
+          <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1566473965997-3de9c817e938?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">THE iX.</h5>
+                  <p class="card-text">Pre-order the new all electric BMW iX now.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1580654712603-eb43273aff33?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">THE NEW BMW M5 CS.</h5>
+                  <p class="card-text">Discover the latest limited edition BMW M model. Interested? Speak to a BMW Centre today or sign-up for the latest news.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1557775209-af7bbe9121df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">THE BMW RANGE.</h5>
+                  <p class="card-text">Now with a low Representative 2.9% APR.
+                    BMW (UK) Ltd is a credit broker not a lender.
+                    Ts&Cs apply.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="https://images.unsplash.com/photo-1516515429572-bf32372f2409?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">YOUR PERFECT BMW COULDN'T BE CLOSER.</h5>
+                  <p class="card-text">Compelling 2.9% APR Representative now available on select BMW Approved Used Cars. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  
+  
+ <br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
